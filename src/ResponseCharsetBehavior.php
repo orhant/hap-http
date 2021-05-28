8707a366ec4c77a086d88afe94e6d134485c2198
Игорь Тарасов <develop@dicr.org>
2021-05-28 15:04:30 +0500

    ResponseCharsetBehavior

---
+++ src/ResponseCharsetBehavior.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * @copyright 2019-2021 Dicr http://dicr.org
+ * @author Igor A Tarasov <develop@dicr.org>
+ * @license BSD-3-Clause
+ * @version 28.05.21 15:03:57
+ */
+
+declare(strict_types = 1);
+namespace dicr\http;
+
+use yii\base\Behavior;
+use yii\httpclient\Client;
+use yii\httpclient\RequestEvent;
+
+use function iconv;
+use function preg_match;
+
+/**
+ * Конвертирует ответ в UTF-8
+ */
+class ResponseCharsetBehavior extends Behavior
+{
+    /**
+     * @inheritdoc
+     */
+    public function events(): array
+    {
+        return [
+            Client::EVENT_AFTER_SEND => '_afterSend'
+        ];
+    }
+
+    /**
+     * Adjust response.
+     *
+     * @param RequestEvent $event
+     * @noinspection PhpMethodNamingConventionInspection
+     */
+    public function _afterSend(RequestEvent $event): void
+    {
+        $response = $event->response;
+        $matches = null;
+        $charset = null;
+
+        // определяем кодировку ответа
+        $contentType = $response->headers->get('content-type');
+        if (($contentType !== null
+                && preg_match('~text/\w+~ui', $contentType)
+                && preg_match('~[\;\s]charset=[\'\"]?(.+?)[\'\"\;\s]*$~ui', $contentType, $matches))
+            || preg_match('~<meta\s+charset=[\'\"](.+?)[\'\"]~ism', $response->content, $matches)
+            || preg_match('~<meta\s+[^\>]+charset=(.+?)[\'\"]~ism', $response->content, $matches)
+        ) {
+            $charset = $matches[1];
+        }
+
+        // перекодируем ответ
+        if ($charset !== null && ! preg_match('~^utf\-?8$~ui', $charset)) {
+            $response->content = iconv($charset, 'utf-8', $response->content);
+        }
+    }
+}
